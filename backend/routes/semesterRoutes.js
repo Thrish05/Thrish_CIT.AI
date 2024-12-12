fad3ce7a11cabae96c72954d82c48bc1ef605249
Thrish <141299284+Thrish05@users.noreply.github.com>
2024-12-12 22:22:59 +0530

    Delete backend directory

--- backend/routes/semesterRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { addSemesterDetails } = require('../controllers/semesterController');  // Correct path for your controller
-const router = express.Router();
-
-// POST route to add semester details
-router.post('/semester-details', addSemesterDetails);
-
-module.exports = router;
